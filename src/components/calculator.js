95341277fae2f76a5a492dbdedba27a6213610c8
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-25 03:48:48 -0700

    Implement logic using events and added logic folders

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
+import calculate from '../logic/calculate';
+import Button from './button';
 
-class Calculator extends Component {
+class Calculator extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      calcObject: {
+        operation: null,
+        total: null,
+        next: null,
+      },
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(buttonName) {
+    const { calcObject } = this.state;
+    const newCalcObject = { ...calcObject, ...calculate(calcObject, buttonName) };
+    this.setState({
+      calcObject: newCalcObject,
+    });
   }
 
   render() {
+    const { calcObject } = this.state;
+    const result = calcObject.next || calcObject.total;
     return (
       <div className="App">
         <div className="calculator">
           <div className="display">
-            0
+            {Number(result)}
           </div>
-          <div className="digits">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button" className="orange">/</button>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="orange">*</button>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="orange">-</button>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="orange">+</button>
-            <button type="button" className="span-two">0</button>
-            <button type="button">.</button>
-            <button type="button" className="orange">=</button>
-          </div>
+          <Button value="AC" onClick={this.onClick} />
+          <Button value="+/-" onClick={this.onClick} />
+          <Button value="%" onClick={this.onClick} />
+          <Button value="÷" className="orange" onClick={this.onClick} />
+          <Button value="7" onClick={this.onClick} />
+          <Button value="8" onClick={this.onClick} />
+          <Button value="9" onClick={this.onClick} />
+          <Button value="x" className="orange" onClick={this.onClick} />
+          <Button value="4" onClick={this.onClick} />
+          <Button value="5" onClick={this.onClick} />
+          <Button value="6" onClick={this.onClick} />
+          <Button value="-" className="orange" onClick={this.onClick} />
+          <Button value="1" onClick={this.onClick} />
+          <Button value="2" onClick={this.onClick} />
+          <Button value="3" onClick={this.onClick} />
+          <Button value="+" className="orange" onClick={this.onClick} />
+          <Button value="0" className="span-two" onClick={this.onClick} />
+          <Button value="." onClick={this.onClick} />
+          <Button value="=" className="orange" onClick={this.onClick} />
         </div>
       </div>
     );
