e349765061d4b5d7e4a33200de4f4c9da029ddb0
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-29 11:58:09 -0700

    Add Router Links

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+
+  <nav className="navBar">
+    <Link to="/">Home</Link>
+    <Link to="App">Calculator</Link>
+    <Link to="Quotes">Quotes</Link>
+  </nav>
+);
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Navbar from './components/Navbar/Navbar';
+import Quotes from './components/Quotes/Quotes';
+import Homepage from './components/Home/Homepage';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/App" element={<App />} />
+        <Route path="/Quotes" element={<Quotes />} />
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
